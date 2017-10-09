ff9c934bd340848161ac398bcb148bc5a63ebcc5
Polybong <urrra03@gmail.com>
2017-10-09 18:38:41 +0300

    modify footer

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
-import { FooterModule } from "./footer/footer.module";
+
 
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
-platform.bootstrapModule(FooterModule);
